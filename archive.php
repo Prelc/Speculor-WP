43c80f43160b8560167baf29519d426e6ad45f2f
Marko Prelec <mareprelc@gmail.com>
2016-10-02 20:58:02 +0200

    Fix template html structure

--- archive.php
+++ archive.php
@@ -14,37 +14,43 @@ get_template_part( 'template-parts/page-header' );
 ?>
 
 	<div class="content-area  container">
-		<main class="site-main">
+		<div class="row">
+			<!-- Main Content -->
+			<main class="col-xs-12  col-lg-8">
+				<?php
+				if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+						/*
+						 * Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_format() );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+					endwhile;
 
-			endwhile;
+					the_posts_pagination( array(
+						'prev_text' => 'previous',
+						'next_text' => 'next',
+					) );
 
-			the_posts_pagination( array(
-				'prev_text' => 'Previous',
-				'next_text' => 'Next',
-			) );
+				else :
 
-		else :
+					get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+				endif; ?>
+			</main>
+			<!-- Sidebar -->
+			<div class="col-xs-12  col-lg-4">
+				<div class="sidebar  sidebar--blog">
+					<?php dynamic_sidebar( 'blog-sidebar' ); ?>
+				</div>
+			</div>
+		</div><!-- .row -->
+	</div><!-- .content-area -->
 
-		endif; ?>
-
-	</main><!-- .site-main -->
-</div><!-- .content-area -->
-
-<?php
-get_footer();
+<?php get_footer(); ?>
--- search.php
+++ search.php
@@ -13,38 +13,44 @@ get_template_part( 'template-parts/page-header' );
 
 ?>
 
-	<section class="content-area  container">
-		<main class="site-main">
+	<div class="content-area  container">
+		<div class="row">
+			<!-- Main Content -->
+			<main class="col-xs-12  col-lg-8">
+				<?php
+				if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
+					<?php
+					/* Start the Loop */
+					while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+						/**
+						 * Run the loop for the search to output the results.
+						 * If you want to overload this in a child theme then include a file
+						 * called content-search.php and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', 'search' );
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+					endwhile;
 
-			endwhile;
+					the_posts_pagination( array(
+						'prev_text' => 'previous',
+						'next_text' => 'next',
+					) );
 
-			the_posts_pagination( array(
-				'prev_text' => 'Previous',
-				'next_text' => 'Next',
-			) );
+				else :
 
-		else :
+					get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+				endif; ?>
+			</main>
+			<!-- Sidebar -->
+			<div class="col-xs-12  col-lg-4">
+				<div class="sidebar  sidebar--blog">
+					<?php dynamic_sidebar( 'blog-sidebar' ); ?>
+				</div>
+			</div>
+		</div><!-- .row -->
+	</div><!-- .content-area -->
 
-		endif; ?>
-
-	</main><!-- .site-main -->
-</section><!-- .content-area -->
-
-<?php
-get_footer();
+<?php get_footer(); ?>
