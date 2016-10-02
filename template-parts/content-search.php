fbdc22c255032bc470fb2b6e48646865dc20e245
Marko Prelec <mareprelc@gmail.com>
2016-10-02 21:04:44 +0200

    Fix content html structure

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,21 +9,40 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="hentry__header">
-		<?php
-			the_title( sprintf( '<h2 class="hentry__title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-			if ( 'post' === get_post_type() ) : ?>
-		<?php
-		endif; ?>
-	</header>
-
-	<div class="hentry__content">
-		<?php the_excerpt(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'article  clearfix' ); ?>>
+	<!-- Post Thumbnail -->
+	<div class="article__featured-image-container">
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'article__featured-image  img-fluid' ) ); ?>
+			</a>
+		<?php endif; ?>
 	</div>
-
-	<footer class="hentry__footer">
-		<!-- Meta -->
-		<?php get_template_part( 'template-parts/meta' ); ?>
-	</footer>
+	<!-- Meta -->
+	<div class="article__meta  meta">
+		<!-- Date -->
+		<a href="<?php the_permalink(); ?>"><time class="meta__item  meta__item--date" datetime="<?php the_time( 'c' ); ?>"><?php echo get_the_date(); ?></time></a>
+		<!-- Categories -->
+		<?php if ( has_category() ) : ?>
+			<!-- Separator -->
+			<span class="meta__separator">/</span>
+			<span class="meta__item  meta__item--categories"><?php the_category( ', ' ); ?></span>
+		<?php endif; ?>
+	</div>
+	<!-- Title -->
+	<?php
+		the_title( '<h2 class="article__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		if ( 'post' === get_post_type() ) : ?>
+	<?php
+	endif; ?>
+	<!-- Content & Read more text -->
+	<div class="article__content">
+		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Read more', 'speculor' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+	</div>
 </article><!-- #post-## -->
