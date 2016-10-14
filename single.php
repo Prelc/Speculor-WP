0c608aa5fb7b284fb7035d58783c87c1dba3edfa
Marko Prelec <mareprelc@gmail.com>
2016-10-14 18:27:16 +0200

    Add post navigation html structure and style

--- single.php
+++ single.php
@@ -38,6 +38,7 @@ get_template_part( 'template-parts/page-header' );
 						</div>
 					</div>
 
+					<!-- Comments -->
 					<?php
 					// If comments are open or we have at least one comment, load up the comment template.
 					if ( comments_open() || get_comments_number() ) :
@@ -54,11 +55,33 @@ get_template_part( 'template-parts/page-header' );
 					<div class="collapse" id="collapseComments">
 						<?php comments_template(); ?>
 					</div>
+					<?php endif; ?>
 
-				<?php
-				endif;
-					endwhile; // End of the loop.
-				?>
+					<!-- Post Navigation -->
+					<?php
+					$prev_post = get_adjacent_post( false, '', false );
+					$next_post = get_adjacent_post( false, '', true );
+					?>
+
+					<div class="post-navigation__container">
+						<a class="post-navigation  post-navigation--previous" href="<?php echo get_permalink( $prev_post ); ?>">
+							<div class="h5  post-navigation__title  post-navigation__title--previous">
+								<?php echo get_the_title( $prev_post ); ?>
+							</div>
+							<div class="h6  post-navigation__text  post-navigation__text--previous">
+								<?php esc_html_e( 'Previous post' , 'speculor' ); ?>
+							</div>
+						</a>
+						<a class="post-navigation  post-navigation--next" href="<?php echo get_permalink( $next_post ); ?>">
+							<div class="h5  post-navigation__title  post-navigation__title--next">
+								<?php echo get_the_title( $next_post ); ?>
+							</div>
+							<div class="h6  post-navigation__text  post-navigation__text--next">
+								<?php esc_html_e( 'Next post' , 'speculor' ); ?>
+							</div>
+						</a>
+					</div>
+				<?php endwhile; // End of the loop. ?>
 			</main>
 			<!-- Sidebar -->
 			<div class="col-xs-12  col-lg-4">
